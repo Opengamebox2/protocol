08c12c99c89e40631499bb66e8434b6e687d4f71
Lauri Niskanen <ape@ape3000.com>
2016-04-25 02:39:28 +0300

    Restructure the protocol module

---
+++ index.js
@@ -0,0 +1,25 @@
+export default {
+  /** Messages that the server broadcasts to all clients. */
+  broadcast: {
+    PLAYER_JOIN: 'PLAYER_JOIN',
+    PLAYER_LEAVE: 'PLAYER_LEAVE',
+    ENTITY_CREATE: 'ENTITY_CREATE',
+    ENTITY_DELETE: 'ENTITY_DELETE',
+    ENTITY_SELECT: 'ENTITY_SELECT',
+    ENTITY_MOVE: 'ENTITY_MOVE',
+  },
+
+  /** Requests that may be sent to the server. */
+  requests: {
+    HANDSHAKE: 'HANDSHAKE',
+    ENTITY_CREATE_REQUEST: 'ENTITY_CREATE_REQUEST',
+    ENTITY_DELETE_REQUEST: 'ENTITY_DELETE_REQUEST',
+    ENTITY_SELECT_REQUEST: 'ENTITY_SELECT_REQUEST',
+    ENTITY_MOVE_REQUEST: 'ENTITY_MOVE_REQUEST',
+  },
+
+  /** Reply messages to requests. */
+  replies: {
+    HANDSHAKE_REPLY: 'HANDSHAKE_REPLY',
+  },
+}
